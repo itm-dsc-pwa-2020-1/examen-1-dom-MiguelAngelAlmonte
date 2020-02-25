58abb8753b819b0f54b157c2a6f3738f5d189154
MiguelAngelAlmonte <61331221+MiguelAngelAlmonte@users.noreply.github.com>
2020-02-25 13:57:55 -0600

    Examen
    dark

---
+++ cajas/java.js
@@ -0,0 +1,14 @@
+function Dark() {
+    document.getElementById("body").className="body2";
+    document.getElementById("caja_encabezado").className="caja_encabezado2";
+    document.getElementById("caja_principal").className="caja_principal2";
+    document.getElementById("caja_inferior").className="caja_inferior2";
+    document.getElementById("caja_1").className="caja_1j";
+    document.getElementById("caja_2").className="caja_2j";
+    document.getElementById("caja_3").className="caja_3j";
+    document.getElementById("caja_4").className="caja_4j";
+    document.getElementById("caja_pie_1").className="caja_pie_1j";
+    document.getElementById("caja_pie_2").className="caja_pie_2j";   
+    document.getElementById("caja_pie_3").className="caja_pie_3j";   
+    
+}
